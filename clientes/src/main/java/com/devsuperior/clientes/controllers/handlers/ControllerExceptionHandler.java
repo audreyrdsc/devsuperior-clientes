ca933a4aedb16534044b018a96404d1fc4bf0f36
audreyrdsc <audreyrdsc@gmail.com>
2025-05-14 20:49:04 -0300

    Criação do Handler para captura de exceções no Controller - Id Integridade e Validação

--- clientes/src/main/java/com/devsuperior/clientes/controllers/handlers/ControllerExceptionHandler.java
+++ clientes/src/main/java/com/devsuperior/clientes/controllers/handlers/ControllerExceptionHandler.java
@@ -1,4 +1,48 @@
 package com.devsuperior.clientes.controllers.handlers;
 
+import com.devsuperior.clientes.dto.CustomError;
+import com.devsuperior.clientes.dto.ValidationError;
+import com.devsuperior.clientes.services.exceptions.DatabaseException;
+import com.devsuperior.clientes.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
 public class ControllerExceptionHandler {
+
+    //Captura da exceção de Id não encontrado
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e , HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    //Captura erros de integridade referencial
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST; //Erro 400
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    //Captura de erros de validação
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValidation(MethodArgumentNotValidException e /*msg da exceção*/, HttpServletRequest request /*caminho da URL*/) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY; //Erro
+        ValidationError err = new ValidationError(Instant.now(), status.value(), "Dados inválidos", request.getRequestURI());
+
+        for (FieldError f : e.getBindingResult().getFieldErrors()) {
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
+        return ResponseEntity.status(status).body(err);
+    }
+
 }
