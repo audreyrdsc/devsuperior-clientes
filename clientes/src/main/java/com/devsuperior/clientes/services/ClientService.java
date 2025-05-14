7f98e0c7a34ba567ef09782849ea356563e7f35e
audreyrdsc <audreyrdsc@gmail.com>
2025-05-14 19:36:25 -0300

    Implementados os métodos update e delete no ClientService

--- clientes/src/main/java/com/devsuperior/clientes/services/ClientService.java
+++ clientes/src/main/java/com/devsuperior/clientes/services/ClientService.java
@@ -3,10 +3,13 @@ package com.devsuperior.clientes.services;
 import com.devsuperior.clientes.dto.ClientDTO;
 import com.devsuperior.clientes.entities.Client;
 import com.devsuperior.clientes.repositories.ClientRepository;
+import com.devsuperior.clientes.services.exceptions.DatabaseException;
 import com.devsuperior.clientes.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.PositiveOrZero;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -47,9 +50,35 @@ public class ClientService {
         return new ClientDTO(entity);
     }
 
+    //Atualização de dados do cliente
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO dto) {
+        try {
+            Client entity = clientRepository.getReferenceById(id); //Instanciar um novo cliente pela referência do Id
+            copyDtoToEntity(dto, entity);
+            entity = clientRepository.save(entity);
+            return new ClientDTO(entity); //reconverter para DTO
+        }
+        catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+    }
 
+    //Apagar um cliente pelo id
+    @Transactional//(propagation = Propagation.SUPPORTS) //
+    public void delete(Long id) {
+        if (!clientRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+        try {
+            clientRepository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Falha de integridade referencial");
+        }
+    }
 
-
+    //Metodo para converter o DTO em Entidade - O construtor de ClientDTO já faz o inverso
     private void copyDtoToEntity(ClientDTO dto, Client entity) {
         entity.setName(dto.getName());
         entity.setCpf(dto.getCpf());
