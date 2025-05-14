1c08e00a9944f5e9b83cc5ab73dceac8851165c2
audreyrdsc <audreyrdsc@gmail.com>
2025-05-14 19:23:47 -0300

    Adicionado métodos de inserção de novo cliente no Service e Controller

--- clientes/src/main/java/com/devsuperior/clientes/controllers/ClientController.java
+++ clientes/src/main/java/com/devsuperior/clientes/controllers/ClientController.java
@@ -2,14 +2,15 @@ package com.devsuperior.clientes.controllers;
 
 import com.devsuperior.clientes.dto.ClientDTO;
 import com.devsuperior.clientes.services.ClientService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -18,16 +19,27 @@ public class ClientController {
     @Autowired
     private ClientService clientService;
 
+    //Rota de busca paginada - todos os clientes
     @GetMapping
     public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
         Page<ClientDTO> dto = clientService.findAll(pageable);
         return ResponseEntity.ok(dto);
     }
 
+    //Rota de busca por id - cliente específico
     @GetMapping(value = "/{id}")
     public ResponseEntity<ClientDTO> findById(@PathVariable long id) {
         ClientDTO dto = clientService.findById(id);
         return ResponseEntity.ok(dto);
+    }
+
+    //Rota de inserção de novo cliente
+    @PostMapping
+    public ResponseEntity<ClientDTO> insert(@Valid @RequestBody ClientDTO dto) {
+        dto = clientService.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 
 
--- clientes/src/main/java/com/devsuperior/clientes/services/ClientService.java
+++ clientes/src/main/java/com/devsuperior/clientes/services/ClientService.java
@@ -4,12 +4,15 @@ import com.devsuperior.clientes.dto.ClientDTO;
 import com.devsuperior.clientes.entities.Client;
 import com.devsuperior.clientes.repositories.ClientRepository;
 import com.devsuperior.clientes.services.exceptions.ResourceNotFoundException;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.PositiveOrZero;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 @Service
@@ -33,6 +36,26 @@ public class ClientService {
                 () -> new ResourceNotFoundException("Recurso não encontrado"));
         ClientDTO dto = new ClientDTO(client);
         return dto;
+    }
+
+    //Inserção de novo cliente - novo id
+    @Transactional
+    public ClientDTO insert(ClientDTO dto) {
+        Client entity = new Client();
+        copyDtoToEntity(dto, entity);
+        entity = clientRepository.save(entity);
+        return new ClientDTO(entity);
+    }
+
+
+
+
+    private void copyDtoToEntity(ClientDTO dto, Client entity) {
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
     }
 
 
