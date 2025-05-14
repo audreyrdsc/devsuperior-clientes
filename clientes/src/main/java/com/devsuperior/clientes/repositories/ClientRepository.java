2cfd18bca98bcb9163fde6b0445d369de32476a4
audreyrdsc <audreyrdsc@gmail.com>
2025-05-14 17:46:46 -0300

    Criação do repository de cliente

--- clientes/src/main/java/com/devsuperior/clientes/repositories/ClientRepository.java
+++ clientes/src/main/java/com/devsuperior/clientes/repositories/ClientRepository.java
@@ -1,4 +1,10 @@
 package com.devsuperior.clientes.repositories;
 
-public class ClientRepository {
+import com.devsuperior.clientes.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+
 }
