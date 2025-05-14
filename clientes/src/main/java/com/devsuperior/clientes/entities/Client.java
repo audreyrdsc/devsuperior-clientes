39cc069658c5e4e934ead1fa705128027bd38ff8
audreyrdsc <audreyrdsc@gmail.com>
2025-05-14 17:26:03 -0300

    Criação da entidade Client

--- clientes/src/main/java/com/devsuperior/clientes/entities/Client.java
+++ clientes/src/main/java/com/devsuperior/clientes/entities/Client.java
@@ -1,4 +1,29 @@
 package com.devsuperior.clientes.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_client")
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(length = 11, unique = true)
+    private String cpf;
+    private Double income;
+    private LocalDate birthDate;
+    private Integer children;
+
 }
